bf87907ad9dfcf07a16f2e6ef30d6fc11d6e21ef
LeiChen <chinadarren@outlook.com>
2015-07-29 18:16:43 +0800

    s

--- src/com/example/FragmentTest/RightFragment.java
+++ src/com/example/FragmentTest/RightFragment.java
@@ -1,7 +1,9 @@
 package com.example.FragmentTest;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,9 +12,26 @@ import android.view.ViewGroup;
  * Created by leichen on 7/28/2015.
  */
 public class RightFragment extends Fragment{
+
+    public static final String TAG = "RightFragment";
+
+    @Override
+    public void onAttach(Activity activity){
+        super.onAttach(activity);
+        Log.d(TAG, "onAttach");
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        Log.d(TAG,"onCreate");
+    }
     @Override
     public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState){
-        View view = inflater.inflate(R.layout.right_fragment,container,false);
+        Log.d(TAG,"onCreateView");
+            View view = inflater.inflate(R.layout.right_fragment,container,false);
         return view;
     }
+
+
+
 }
